a1898d904316cb4cb36f9ccd31cf3d1b4cd8f30e
LinasKeller-cmd <Linas_Keller@sluz.ch>
2026-09-11 08:05:43 +0200

    never

--- NutriBaseProject/src/App.vue
+++ NutriBaseProject/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-  import Navbar from './components/Navbar'
+  import Navbar from './components/navbar.vue'
+  import Product from './components/product.vue'
 </script>
 
 <template>
+
   <div>
+    <Navbar />
     <nav>
 
     </nav>
   </div>
+  <main>
+    <Product />
+  </main>
 </template>
---
+++ NutriBaseProject/src/components/product.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import navbar from './navbar.vue'
+
+</script>
+
+<template>
+  <div>
+    <nav>
+
+    </nav>
+  </div>
+  <main>
+    <input v-model="message" type="text" placeholder="favorites">
+    <button @click="console.log(message)">Submit</button>
+    <a href="product.vue">product</a>
+  </main>
+
+</template>
