b19837fe6ae91b00b72198a5f0b7bc158d438bf0
StefanF2 <Stefan_Filipov@sluz.ch>
2026-09-17 10:52:56 +0200

    Ui von Best results hinzugefügt

--- NutriBaseProject/src/components/Home.vue
+++ NutriBaseProject/src/components/Home.vue
@@ -1,6 +1,55 @@
 <script setup>
 import {ref} from "vue";
+import Result from "./Result.vue";
 const input = ref();
+const searchedbar = ref(false);
+function showResults() {
+  searchedbar.value = true;
+}
+
+const tests = ref([
+  {
+    id: 1,
+    title: "Produkt1",
+    image: 'src/assets/vue.svg'
+  },
+  {
+    id: 2,
+    title: "Produkt2",
+    image: 'src/assets/Wasser.jpg'
+  },
+  {
+    id: 3,
+    title: "Produkt3",
+    image: 'src/assets/Wasser.jpg'
+  },
+  {
+    id: 4,
+    title: "Produkt4",
+    image: 'src/assets/Wasser.jpg'
+  },
+  {
+    id: 5,
+    title: "Produkt5",
+    image: 'src/assets/Wasser.jpg'
+  },
+  {
+    id: 6,
+    title: "Produkt6",
+    image: 'src/assets/vue.svg'
+  },
+  {
+    id: 7,
+    title: "Produkt7",
+    image: 'src/assets/vue.svg'
+  },
+  {
+    id: 8,
+    title: "Produkt8",
+    image: 'src/assets/vue.svg'
+  },
+])
+
 </script>
 
 <template>
@@ -9,15 +58,23 @@ const input = ref();
       <h1 id="home__header">Nutri-Base</h1>
       <h2 id="home__info">A Database to search for Product information</h2>
         <div class="home__search__container">
-          <input  id="home__searchbar" type="search" placeholder="Search..." v-model.lazy="input"/>
-          <button id="home__button" type="submit"><img class="home__button" src="../assets/searchbar_icon.png" alt="icon"></button>
+          <input  id="home__searchbar" type="search" placeholder="Search..." v-model.lazy="input" @keyup.enter="showResults"/>
+          <button id="home__button" type="submit"><img class="home__button" src="../assets/searchbar_icon.png" alt="icon" @click="showResults"></button>
         </div>
-      <p>{{input}}</p>
-    </div>
-    <div class="result">
-      <h2 id="result__header">Best results: </h2>
-    </div>
 
+    </div>
+    <div class="result" v-show="searchedbar">
+      <h2 id="result__header" >Best results: </h2>
+
+      <div class="result__grid">
+        <Result
+          v-for="test in tests"
+          :key="test.id"
+          :title="test.title"
+          :image="test.image"
+          />
+      </div>
+    </div>
   </main>
   <RouterView />
 </template>
@@ -69,14 +126,20 @@ body{
   cursor: pointer;
   background-color: transparent;
 }
-.result{
-  display: flex;
-  margin: 0 15rem 0 15rem;
+.result {
+  margin: 0 15rem;
+}
+
+.result__grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+  color: white;
 }
 #result__header{
   font-size: 2rem;
   font-family: "Century Gothic", Helvetica, sans-serif;
-  display: none;
+  padding-bottom: 1rem;
 }
 
 </style>
---
+++ NutriBaseProject/src/components/Result.vue
@@ -0,0 +1,54 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  image: String,
+})
+</script>
+
+<template>
+  <button class="results__item">
+    <div class="results__image">
+      <img :src="props.image" alt=""/>
+    </div>
+    <div class="results__title">
+      {{ props.title }}
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.results__item {
+  width: 100%;
+  aspect-ratio: 1 / 1;
+
+  border: 3px solid black;
+  box-sizing: border-box;
+  background-color: white;
+  cursor:pointer;
+  border-radius: 1rem;
+
+  display: grid;
+  grid-template-rows: 1fr auto;
+  overflow: hidden;
+}
+.results__image {
+  min-height: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.results__image img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+
+.results__title {
+  font-size: 2.5rem;
+  padding: 5px;
+  font-family: "Century Gothic", Helvetica, sans-serif;
+  font-weight: bold;
+}
+
+</style>
